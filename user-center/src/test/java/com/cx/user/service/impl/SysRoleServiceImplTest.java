b738a730022991ade9267613de24b176b4dd74a0
chenxin1215 <154660165@qq.com>
2020-10-26 23:53:03 +0800

    fix

--- user-center/src/test/java/com/cx/user/service/impl/SysRoleServiceImplTest.java
+++ user-center/src/test/java/com/cx/user/service/impl/SysRoleServiceImplTest.java
@@ -1,6 +1,7 @@
 package com.cx.user.service.impl;
 
-import com.cx.utils.util.JsonUtil;
+import java.util.Set;
+
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,8 +9,7 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import com.cx.user.service.IAPIUserRoleService;
-
-import java.util.Set;
+import com.cx.utils.util.JsonUtil;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
